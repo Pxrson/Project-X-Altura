5526b5130a27335812fcc9f779fef38441e47c76
pxrson <159809309+Pxrson@users.noreply.github.com>
2025-06-20 16:32:45 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -2,12 +2,37 @@ local StarterGui = game:GetService("StarterGui")
 local detectScriptUrl = game:HttpGet("https://raw.githubusercontent.com/Pxrson/Altura-Hub/refs/heads/main/Detect%20Game.lua")
 
 if detectScriptUrl and detectScriptUrl ~= "" then
-    loadstring(detectScriptUrl)()
+    local detectGame = loadstring(detectScriptUrl)
+    if detectGame then
+        local gameId = detectGame()
+        if type(gameId) == "string" and gameId ~= "" then
+        else
+            StarterGui:SetCore("SendNotification", {
+                Title = "Altura Hub",
+                Text = "🚀 Game not supported, loading selector...",
+                Duration = 3
+            })
+            local selectorScriptUrl = game:HttpGet("https://raw.githubusercontent.com/Pxrson/Altura-Hub/refs/heads/main/Selector.lua")
+            local selector = loadstring(selectorScriptUrl)
+            selector()
+        end
+    else
+        StarterGui:SetCore("SendNotification", {
+            Title = "Altura Hub",
+            Text = "🚀 Problem with game detection, loading selector...",
+            Duration = 3
+        })
+        local selectorScriptUrl = game:HttpGet("https://raw.githubusercontent.com/Pxrson/Altura-Hub/refs/heads/main/Selector.lua")
+        local selector = loadstring(selectorScriptUrl)
+        selector()
+    end
 else
     StarterGui:SetCore("SendNotification", {
         Title = "Altura Hub",
-        Text = "🚀 Not a game for Altura Hub, loading selector...",
+        Text = "🚀 Unable to detect game, loading selector...",
         Duration = 3
     })
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Pxrson/Altura-Hub/refs/heads/main/Selector.lua"))()
+    local selectorScriptUrl = game:HttpGet("https://raw.githubusercontent.com/Pxrson/Altura-Hub/refs/heads/main/Selector.lua")
+    local selector = loadstring(selectorScriptUrl)
+    selector()
 end
