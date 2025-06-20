0e65d315f2a208e0d2557f165c3235081fe544ab
pxrson <159809309+Pxrson@users.noreply.github.com>
2025-06-20 16:06:19 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -8,13 +8,17 @@ if detectScriptUrl and detectScriptUrl ~= "" then
         Text = "✅ Supported game detected, loading script...",
         Duration = 3
     })
-    wait(1)
-    loadstring(detectScriptUrl)()
+
+    wait(0.5)
+
+    local scriptSource = game:HttpGet(detectScriptUrl)
+    loadstring(scriptSource)()
 else
     StarterGui:SetCore("SendNotification", {
         Title = "Altura Hub",
         Text = "🚀 Not a game for Altura Hub, loading selector...",
         Duration = 3
     })
+
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Pxrson/Altura-Hub/refs/heads/main/Selector.lua"))()
 end
