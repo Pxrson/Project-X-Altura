cbcac4876dc7e9b530f3d453c6dada2f70e9e874
pxrson <159809309+Pxrson@users.noreply.github.com>
2025-07-26 02:28:11 -0500

    Update Detect Game.lua

--- Detect Game.lua
+++ Detect Game.lua
@@ -1,11 +1,11 @@
 local placeId = game.PlaceId
 
 if placeId == 142823291 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Pxrson/Altura-Hub/refs/heads/main/Murder%20Mystery%202.lua", true))() -- Murder Mystery 2
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Pxrson/Project-X-Altura/refs/heads/main/Main.lua",true))() -- project x altura
 elseif placeId == 8737899170 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Pxrson/oMega-Obby/refs/heads/main/Script", true))() -- oMega Obby
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Pxrson/oMega-Obby/refs/heads/main/Script", true))() -- omega obby
 elseif placeId == 125822183289695 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Pxrson/Wall-Hop-Obby/refs/heads/main/Script.lua", true))() -- Wall Hop Obby
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Pxrson/Wall-Hop-Obby/refs/heads/main/Script.lua", true))() -- wall hop obby
 else
     warn("This game is not supported by Altura Hub.")
 end
