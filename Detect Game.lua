c657fb12c4091e2a8bfcf22bee3da0be5dd56f69
pxrson <159809309+Pxrson@users.noreply.github.com>
2025-07-27 19:27:19 -0500

    Delete Detect Game.lua

--- Detect Game.lua
+++
@@ -1,7 +0,0 @@
-local placeId = game.PlaceId
-
-if placeId == 142823291 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Pxrson/Project-X-Altura/refs/heads/main/Main.lua",true))() -- project x altura
-else
-    warn("This game is not supported by Project X Altura")
-end
