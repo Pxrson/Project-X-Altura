126498d8ed74e7bb86e32236062a81c1d048b760
pxrson <159809309+Pxrson@users.noreply.github.com>
2025-08-01 14:18:19 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,4 +1,4 @@
--- what are you doing here???
+-- what are you doing here??? go cut the grass kiddo
 -- discord: .pxrson
 if game.PlaceId == 142823291 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Pxrson/Project-X-Altura/refs/heads/main/Murder%20Mystery%202.lua", true))()
