c634c92ebe99b5a945fab517c7051a7e1731e55c
pxrson <159809309+Pxrson@users.noreply.github.com>
2025-08-01 13:57:41 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -2,6 +2,4 @@
 -- discord: .pxrson
 if game.PlaceId == 142823291 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Pxrson/Project-X-Altura/refs/heads/main/Murder%20Mystery%202.lua", true))()
-else
-    game.Players.LocalPlayer:Kick("This script is only for Murder Mystery 2")
 end
